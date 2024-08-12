3aad49dce19d0224dfbf2d493740707c51275d1f
Milan Julinac <milanjulinac996@gmail.com>
2024-08-12 23:54:51 +0200

    updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './utility/env/env.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvModule } from './utility/env/env.module';
+import { EnvService } from './utility/env/env.service';
+import { getDbConfig } from './config/database';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (config) => envSchema.parse(config),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [EnvModule],
+      inject: [EnvService],
+      useFactory: (envService: EnvService) => getDbConfig(envService),
+    }),
+    EnvModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
